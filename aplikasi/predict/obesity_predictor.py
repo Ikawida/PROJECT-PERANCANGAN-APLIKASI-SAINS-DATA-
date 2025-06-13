b2c9d53a09827c5644e8552f32bd3c7b53fd8393
Dill thafa <161497877+Dillthf@users.noreply.github.com>
2025-06-13 10:00:47 +0700

    Update obesity_predictor.py

--- aplikasi/predict/obesity_predictor.py
+++ aplikasi/predict/obesity_predictor.py
@@ -9,13 +9,21 @@ class ObesityPredictor:
         # Memuat label encoder dari file
         self.label_encoder = joblib.load(encoder_path)
 
-    # Method untuk melakukan prediksi pada data 
+    # Method untuk melakukan prediksi pada data input X (DataFrame)
     def predict(self, X):
+        # Data fitur yang dibutuhkan untuk prediksi
         required_features = ['Age', 'Gender', 'Height', 'Weight', 'BMI', 'PhysicalActivityLevel']
+
+        # Memastikan semua fitur yang dibutuhkan tersedia dalam input
         if not all(col in X.columns for col in required_features):
             raise ValueError(f"Missing required features. Required: {required_features}")
 
+        # Melakukan prediksi kategori (dalam bentuk label numerik)
         prediction = self.model.predict(X)
+        # Mengambil probabilitas dari setiap kelas prediksi
         probabilities = self.model.predict_proba(X)
+        # Mengubah label numerik kembalu ke label asli menggunakan label encoder
         label = self.label_encoder.inverse_transform(prediction)
+
+        # Mengembalikan label prediksi (nama kategori) dan probabilitasnya
         return label[0], probabilities[0]
